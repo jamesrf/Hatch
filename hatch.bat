b1a8ec4952d3d4640d2e80e66c14cca2065db6a0
Jason Boyer <JBoyer@library.in.gov>
2018-01-25 23:20:17 -0500

    LP1743604: Try to find Java automatically
    Starting sometime in the 1.8 timeframe Java
    installations on Windows are making use of unix-
    style symlinks available in NTFS to keep the
    most reecnt version installed available at a
    known location. We may as well look there if
    the path isn't set correctly for some reason.
    
    Signed-off-by: Jason Boyer <JBoyer@library.in.gov>
    Signed-off-by: Chris Sharp <csharp@georgialibraries.org>
    Signed-off-by: Galen Charlton <gmc@equinoxinitiative.org>

--- hatch.bat
+++ hatch.bat
@@ -2,6 +2,11 @@
 REM Windows Hatch Execution Script
 REM @echo off required for STDIO to work with the browser.
 
+REM NOTE: Do not EVER set ERRORLEVEL for any reason; it's a passthrough variable that takes on the current value
+REM of the errorlevel return status but if you assign to it that magic passthrough is broken and it's just set to that value.
+REM This is necessary to do sensible comparisons against it. (such as EQU) The 'if errorlevel' construct is completely bananas.
+REM Also: automatic path testing only looks for 'java' - if you don't have the JDK in your path you'll need to fix that yourself.
+
 REM Assume java executables are in our path
 SET JAVA=java
 SET JAVAC=javac
@@ -13,6 +18,22 @@ REM SET JAVA=%JAVA_HOME%\bin\java
 REM SET JAVAC=%JAVA_HOME%\bin\javac
 REM SET JAR=%JAVA_HOME%\bin\jar
 
+REM Is anyone there?
+%JAVA% --dry-run -cp "lib\*" org.evergreen_ils.hatch.Hatch 2>nul
+
+IF %ERRORLEVEL% EQU 0 GOTO Huzzah
+
+REM Are you still there?
+SET JAVA=%PROGRAMDATA%\Oracle\Java\javapath\java
+%JAVA% --dry-run -cp "lib\*" org.evergreen_ils.hatch.Hatch 2>nul
+
+IF %ERRORLEVEL% EQU 0 GOTO Huzzah
+
+REM I don't blame you
+EXIT %ERRORLEVEL%
+
+REM There you are.
+:Huzzah
 
 IF "%1" == "compile" (
 
@@ -42,4 +63,3 @@ IF "%1" == "compile" (
 
     )
 )
-
